57c7c5667f089d53bdea3614e6e147a02662a0a0
Bonetik <79835601+Bonetik@users.noreply.github.com>
2021-03-27 10:44:24 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+var mySwiper2 = new Swiper('.swiper-container-int', {
+  pagination: {
+    el: '.swiper-int',
+    clickable: true,
+  },
+});
